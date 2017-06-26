7af17e75d04ee2d1bb1016fcb5e3dce25db7b326
Sam Leo <sandyleo26@gmail.com>
2017-06-26 11:45:37 +1000

    add sobelx and s channel thresholding

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import cv2
 import glob
 
 def get_mtx_dist():
+    '''
+    Get calibratoin matrix and distortion coefficents
+    '''
     objPoints = []
     imgPoints = []
     objP = np.zeros((6*9,3), np.float32)
@@ -28,6 +31,7 @@ def get_mtx_dist():
 ## Run once and save the cameraMatrix and distCoeffs
 # mtx, dist = get_mtx_dist()
 # np.savez('./calibration_data.npz', mtx=mtx, dist=dist)
+
 calibration_data = np.load('./calibration_data.npz')
 mtx, dist = calibration_data['mtx'], calibration_data['dist']
 
@@ -40,4 +44,23 @@ mtx, dist = calibration_data['mtx'], calibration_data['dist']
 # plt.show()
 
 
+img = mpimg.imread('./test_images/straight_lines1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+kernel_size = 5
+sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, kernel_size)
+absolute_sobelx = np.absolute(sobelx)
+scaled_sobelx = 255*absolute_sobelx/np.max(absolute_sobelx)
+sobelx_binary = np.zeros_like(gray)
+sobelx_thresh = (30, 100)
+sobelx_binary[(scaled_sobelx > sobelx_thresh[0]) & (scaled_sobelx < sobelx_thresh[1])] = 1
 
+hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+schannel = hls[:,:,2]
+schannel_binary = np.zeros_like(schannel)
+thresh = (30,100)
+schannel_binary[(schannel > thresh[0]) & (schannel < thresh[1])] = 1
+
+fig, axes = plt.subplots(1,2,figsize=(10,6))
+axes[0].imshow(sobelx_binary, cmap='gray')
+axes[1].imshow(schannel_binary, cmap='gray')
+plt.show()
